79eab0f3c72e8e777dbcb36d7dcb064e67a9e8e0
Sneza122 <sdragicevic122it@raf.rs>
2025-04-29 13:24:06 +0200

    Kreirana osnovna navigacija sa TabbedPage za MainPage

--- StudentskeObevezeApp/StudentskeObevezeApp/MainPage.xaml.cs
+++ StudentskeObevezeApp/StudentskeObevezeApp/MainPage.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace StudentskeObevezeApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();  // Ovaj poziv treba da bude validan
         }
     }
 }
