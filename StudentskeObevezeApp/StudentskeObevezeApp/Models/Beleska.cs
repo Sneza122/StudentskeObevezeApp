4d1546d7caadfebd4873fea4ea8fa81fe8e6cf74
ASampor <samporanja@gmail.com>
2025-05-07 16:37:21 +0200

    Dodata Beleška u models

---
+++ StudentskeObevezeApp/StudentskeObevezeApp/Models/Beleska.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentskeObevezeApp.Models
+{
+    public class Beleska
+    {
+        public string Tekst { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
