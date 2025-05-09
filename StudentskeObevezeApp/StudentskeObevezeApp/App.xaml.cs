9c08872bcaa818bf79d51d0c48ca078bc07deb49
Sneza122 <sdragicevic122it@raf.rs>
2025-05-09 10:27:59 +0200

    Doradjen izgled stranice dnevni zadaci

--- StudentskeObevezeApp/StudentskeObevezeApp/App.xaml.cs
+++ StudentskeObevezeApp/StudentskeObevezeApp/App.xaml.cs
@@ -30,52 +30,45 @@ namespace StudentskeObevezeApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
-        }
-
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DbFileName);
-
-        
-
             var baza = Database;
 
             Task.Run(async () =>
             {
                 var postojeZadaci = await baza.GetItemsAsync();
 
-                // Dodajemo testne zadatke samo ako baza nije prazna
                 if (postojeZadaci.Count == 0)
                 {
                     var zadaci = new List<Models.Zadaci>
-        {
-            new Models.Zadaci
             {
-                Naziv = "Učiti za ispit",
-                Opis = "Spremiti SQL upite",
-                Datum = DateTime.Now.AddDays(1),
-                Prioritet = Models.PrioritetZadatka.Visok,
-                Zavrsen = false,
-                PodsetnikUkljucen = false
-            },
-            new Models.Zadaci
-            {
-                Naziv = "Napraviti prezentaciju",
-                Opis = "Završiti slajdove za seminar",
-                Datum = DateTime.Now.AddDays(2),
-                Prioritet = Models.PrioritetZadatka.Srednji,
-                Zavrsen = false,
-                PodsetnikUkljucen = true
-            },
-            new Models.Zadaci
-            {
-                Naziv = "Vežbati kodiranje",
-                Opis = "Rad na zadacima iz OOP-a",
-                Datum = DateTime.Now.AddDays(3),
-                Prioritet = Models.PrioritetZadatka.Nizak,
-                Zavrsen = true,
-                PodsetnikUkljucen = false
-            }
-        };
+                new Models.Zadaci
+                {
+                    Naziv = "Učiti za ispit",
+                    Opis = "Spremiti SQL upite",
+                    Datum = DateTime.Now.AddDays(1),
+                    Prioritet = Models.PrioritetZadatka.Visok,
+                    Zavrsen = false,
+                    PodsetnikUkljucen = false
+                },
+                new Models.Zadaci
+                {
+                    Naziv = "Napraviti prezentaciju",
+                    Opis = "Završiti slajdove za seminar",
+                    Datum = DateTime.Now.AddDays(2),
+                    Prioritet = Models.PrioritetZadatka.Srednji,
+                    Zavrsen = false,
+                    PodsetnikUkljucen = true
+                },
+                new Models.Zadaci
+                {
+                    Naziv = "Vežbati kodiranje",
+                    Opis = "Rad na zadacima iz OOP-a",
+                    Datum = DateTime.Now.AddDays(3),
+                    Prioritet = Models.PrioritetZadatka.Nizak,
+                    Zavrsen = true,
+                    PodsetnikUkljucen = false
+                }
+            };
 
                     foreach (var zad in zadaci)
                     {
@@ -84,12 +77,14 @@ namespace StudentskeObevezeApp
                 }
             }).Wait();
 
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#E91E63"),
+                BarTextColor = Color.White
+            };
 
-
-
-            InitializeComponent();
-
-            MainPage = new NavigationPage(new MainPage()); // <- OVO je važno!
         }
+
+
     }
 }
